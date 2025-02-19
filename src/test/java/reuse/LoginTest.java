fb1c839f17b613766fd58d199b0e1aaced70437a
mohamedtawfek05354 <mohamedtawfeek05354@gmail.com>
2025-02-19 10:19:39 +0200

    Update LoginTest.java

--- src/test/java/reuse/LoginTest.java
+++ src/test/java/reuse/LoginTest.java
@@ -16,14 +16,6 @@ public class LoginTest extends BaseTest {
 //        lo.selectCashier("2");
 //        lo.selectStore("3");
         lo.selection();
-        //assert_correct();
+      
     }
-
-//    public void assert_correct(){
-//        WebDriverWait wait= new WebDriverWait(BaseTestDriver, Duration.ofSeconds(1000));
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/div/h1")));
-//        String welcomeMessage=BaseTestDriver.findElement(By.xpath("/html/body/div/div/div/h1")).getText();
-//        Assert.assertEquals(welcomeMessage, " Dashboard");
-//        System.out.println(" you are in Dashboard page");
-//    }
 }
